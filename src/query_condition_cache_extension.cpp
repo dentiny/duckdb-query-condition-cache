1003f0920de4dcdf4f1a6d78e9ea87802009e761
YunTang(Andrew) Chang <candrew9213@gmail.com>
2026-02-24 20:19:56 +0800

    Rename quack template to query_condition_cache

---
+++ src/query_condition_cache_extension.cpp
@@ -0,0 +1,37 @@
+#define DUCKDB_EXTENSION_MAIN
+
+#include "query_condition_cache_extension.hpp"
+
+#include "duckdb.hpp"
+
+namespace duckdb {
+
+namespace {
+void LoadInternal(ExtensionLoader &loader) {
+	// Functions and optimizer will be registered in subsequent PRs.
+}
+} // namespace
+
+void QueryConditionCacheExtension::Load(ExtensionLoader &loader) {
+	LoadInternal(loader);
+}
+
+std::string QueryConditionCacheExtension::Name() {
+	return "query_condition_cache";
+}
+
+std::string QueryConditionCacheExtension::Version() const {
+#ifdef EXT_VERSION_QUERY_CONDITION_CACHE
+	return EXT_VERSION_QUERY_CONDITION_CACHE;
+#else
+	return "";
+#endif
+}
+
+} // namespace duckdb
+
+extern "C" {
+DUCKDB_CPP_EXTENSION_ENTRY(query_condition_cache, loader) {
+	duckdb::LoadInternal(loader);
+}
+}
