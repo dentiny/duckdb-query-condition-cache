51036ab8e772597111bb06994d0ff90cc177f15a
YunTang(Andrew) Chang <150593090+Andrewtangtang@users.noreply.github.com>
2026-02-24 22:47:31 +0800

    Rename quack template to query_condition_cache (#1)

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,10 +1,6 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
-# Extension from this repo
-duckdb_extension_load(quack
+duckdb_extension_load(query_condition_cache
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
-
-# Any extra extensions that should be built
-# e.g.: duckdb_extension_load(json)
