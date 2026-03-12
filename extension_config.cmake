7243ad1b9776a1bf1f5bc1642710eac9913efb1a
peterxcli <peterxcli@gmail.com>
2026-03-12 18:46:25 +0800

    Add necessary extension for complete test suite

--- extension_config.cmake
+++ extension_config.cmake
@@ -4,3 +4,13 @@ duckdb_extension_load(query_condition_cache
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
+
+duckdb_extension_load(icu)
+duckdb_extension_load(json)
+duckdb_extension_load(tpch)
+duckdb_extension_load(tpcds)
+duckdb_extension_load(autocomplete)
+duckdb_extension_load(httpfs
+    GIT_URL https://github.com/duckdb/duckdb-httpfs
+    GIT_TAG 74f954001f3a740c909181b02259de6c7b942632
+)
