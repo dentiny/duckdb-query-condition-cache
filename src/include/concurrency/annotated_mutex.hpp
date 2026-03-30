e8224ec1ebe38d0c0b14c2110b0053e9e2bd2a8b
dentiny <dentinyhao@gmail.com>
2026-03-30 00:12:43 -0700

    Add thread annoatation (#47)

---
+++ src/include/concurrency/annotated_mutex.hpp
@@ -0,0 +1,31 @@
+// Wrapper around mutex types within standard libraries.
+// See: https://clang.llvm.org/docs/ThreadSafetyAnalysis.html#mutex-h
+
+#pragma once
+
+#include <mutex>
+
+#include "concurrency/internal/mutex_impl.hpp"
+#include "concurrency/thread_annotation.hpp"
+
+namespace duckdb {
+namespace concurrency {
+
+class DUCKDB_CAPABILITY("mutex") mutex : public internal::mutex_impl_t<mutex> {
+private:
+	using Impl = internal::mutex_impl_t<mutex>;
+
+public:
+	void lock() DUCKDB_ACQUIRE() {
+		Impl::lock();
+	}
+	void unlock() DUCKDB_RELEASE() {
+		Impl::unlock();
+	}
+	bool try_lock() DUCKDB_TRY_ACQUIRE(true) {
+		return Impl::try_lock();
+	}
+};
+
+} // namespace concurrency
+} // namespace duckdb
